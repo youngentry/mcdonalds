15bad87d51fbe9e5145805a4196b00d5f1b69acb
youngentry <gentry_@naver.com>
2022-07-05 16:59:18 +0900

    20220705 foods

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function () {
   video(".bgVideo01", "https://youtu.be/u2hLpzjQgvc", ".slider01", 16, 30);
   video(".bgVideo02", "https://youtu.be/MNL6IMZEJ0g", ".slider02", 1, 15);
   video(".bgVideo03", "https://youtu.be/MyVoER9Q-xk", ".slider03", 16, 30);
-  $(".mainSlider").on("reInit afterChange", function (e, s, c) {
+  $(".mainSlider").on("afterChange", (e, s, c) => {
     //$(".bgVideo").YTPPause();
     $(`.bgVideo0${c + 1}`).YTPPlay();
     // $(`.bgVideo0${((c + 1) % 3) + 1}`).YTPPause();
@@ -45,19 +45,19 @@ $(function () {
     fade: true,
   });
 
-  $(".sliderBtn").on("click", function () {
-    $(this).toggleClass("on");
+  $(".sliderBtn").on("click", () => {
+    $(".sliderBtn").toggleClass("on");
     $(".sliderBtn i:first-child").YTPMute();
     $(".sliderBtn i:last-child").YTPUnmute();
   });
 
-  $(".slider-m").slick({
-    asNavFor: ".slider-s",
+  $(".mealSliderM").slick({
+    asNavFor: ".mealSliderS",
     slidesToShow: 1,
     fade: true,
   });
-  $(".slider-s").slick({
-    asNavFor: ".slider-m",
+  $(".mealSliderS").slick({
+    asNavFor: ".mealSliderM",
     slidesToShow: 5,
     swipeToSlide: true,
   });
@@ -75,4 +75,16 @@ $(function () {
   $(".rowSlider03").slick({
     slidesToShow: 3,
   });
+
+  $(".textSlider .slick-prev").on("click", () => {
+    $(".rowSlider01").slick("slickPrev");
+    $(".rowSlider02").slick("slickPrev");
+    $(".rowSlider03").slick("slickPrev");
+  });
+  $(".textSlider .slick-next").on("click", () => {
+    $(".rowSlider01").slick("slickNext");
+    $(".rowSlider02").slick("slickNext");
+    $(".rowSlider03").slick("slickNext");
+  });
+  $(".textSlider .slick-next").on("click", () => {});
 });
