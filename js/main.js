d3b0442223f33f62137931a88a6c2d735f5a2ec9
youngentry <90388461+youngentry@users.noreply.github.com>
2022-07-05 23:23:02 +0900

    220705 mc

--- js/main.js
+++ js/main.js
@@ -10,11 +10,11 @@ $(function () {
     }
   });
 
-  function video(videoClass, videoURL, containmentClass, startAtTime, stopAtTime) {
+  function video(videoClass, videoURL, containmentClass, startAtTime, stopAtTime, playType) {
     $(videoClass).YTPlayer({
       videoURL: videoURL,
       containment: containmentClass,
-      autoPlay: false,
+      autoPlay: playType,
       mute: true,
       startAt: 0,
       stopAt: stopAtTime,
@@ -25,29 +25,16 @@ $(function () {
       startAt: startAtTime,
     });
   }
-  video(".bgVideo01", "https://youtu.be/u2hLpzjQgvc", ".slider01", 16, 30);
-  video(".bgVideo02", "https://youtu.be/MNL6IMZEJ0g", ".slider02", 1, 15);
-  video(".bgVideo03", "https://youtu.be/MyVoER9Q-xk", ".slider03", 16, 30);
-  $(".mainSlider").on("afterChange", (e, s, c) => {
-    //$(".bgVideo").YTPPause();
-    $(`.bgVideo0${c + 1}`).YTPPlay();
-    // $(`.bgVideo0${((c + 1) % 3) + 1}`).YTPPause();
-    // $(`.bgVideo0${((c + 2) % 3) + 1}`).YTPPause();
 
-    // if (c == 0) {
-    //   $(".bgVideo01").YTPPlay();
-    //   $(".bgVideo02").YTPPause();
-    //   $(".bgVideo03").YTPPause();
-    // } else if (c == 1) {
-    //   $(".bgVideo01").YTPPause();
-    //   $(".bgVideo02").YTPPlay();
-    //   $(".bgVideo03").YTPPause();
-    // } else if (c == 2) {
-    //   $(".bgVideo01").YTPPause();
-    //   $(".bgVideo02").YTPPause();
-    //   $(".bgVideo03").YTPPlay();
-    // }
+  video(".bgVideo01", "https://youtu.be/u2hLpzjQgvc", ".slider01", 16, 30, true);
+  video(".bgVideo02", "https://youtu.be/MNL6IMZEJ0g", ".slider02", 1, 15, false);
+  video(".bgVideo03", "https://youtu.be/MyVoER9Q-xk", ".slider03", 16, 30, false);
+  $(".mainSlider").on("afterChange", (e, s, c) => {
+    $(`.bgVideo0${c + 1}`).YTPPlay();
+    $(`.bgVideo0${((c + 1) % 3) + 1}`).YTPPause();
+    $(`.bgVideo0${((c + 2) % 3) + 1}`).YTPPause();
   });
+
   $(".mainSlider").slick({
     arrows: false,
     dots: true,
@@ -56,10 +43,8 @@ $(function () {
     fade: true,
   });
 
-  $(".sliderBtn").on("click", () => {
-    $(".sliderBtn").toggleClass("on");
-    $(".sliderBtn i:first-child").YTPMute();
-    $(".sliderBtn i:last-child").YTPUnmute();
+  $(".sliderBtn").on("click", function () {
+    $(this).toggleClass("on");
   });
 
   $(".mealSliderM").slick({
